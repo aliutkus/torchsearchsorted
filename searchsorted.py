3af635c510a75435318528cec57298a9c611d9af
Antoine Liutkus <antoine.liutkus@inria.fr>
2018-08-16 13:27:42 +0200

    fixed a bug in the dimension checking

--- searchsorted.py
+++ searchsorted.py
@@ -7,15 +7,16 @@ def searchsorted(a, v, out=None):
     assert len(a.shape) == 2, "input `a` must be 2-D."
     assert len(v.shape) == 2, "input `v` mus(t be 2-D."
     assert (a.shape[0] == v.shape[0]
-            or a.shape[1] == 1
-            or v.shape[1] == 1), ("`a` and `v` must have the same number of "
+            or a.shape[0] == 1
+            or v.shape[0] == 1), ("`a` and `v` must have the same number of "
                                   "rows or one of them must have only one ")
 
     if out is not None:
         assert out.shape == v.shape, ("If the output tensor is provided, its "
                                       "shape must match that of `v`.")
     else:
-        out = torch.zeros_like(v)
+        out = torch.zeros((max(a.shape[0], v.shape[0]), v.shape[1]),
+                          dtype=v.dtype, layout=v.layout, device=v.device)
 
     searchsorted_cuda_wrapper(a, v, out)
     return out
