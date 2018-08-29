fbae7a8db862e7ead5986e023e0f9e6bae08bd50
Antoine Liutkus <antoine.liutkus@inria.fr>
2018-08-29 07:20:58 +0200

    modified description

--- searchsorted/searchsorted.py
+++ searchsorted/searchsorted.py
@@ -3,6 +3,15 @@ from .cusearchsorted import searchsorted_cuda_wrapper
 
 
 def searchsorted(a, v, out=None):
+    """Implements a function `searchsorted(a, v)` that works just like the
+    numpy version except that `a` and `v` are matrices.
+    * `a` is of shape either `(1, ncols_a)` or `(nrows, ncols_a)`
+    * `b` is of shape either `(1, ncols_v)` or `(nrows, ncols_v)`.
+
+    the output is of size as `(nrows, ncols_v)`. Only works with pytorch
+    tensors that are already on the GPU.
+
+    """
     assert a.is_cuda and v.is_cuda, "Input tensors must all be on the GPU"
     assert len(a.shape) == 2, "input `a` must be 2-D."
     assert len(v.shape) == 2, "input `v` mus(t be 2-D."
