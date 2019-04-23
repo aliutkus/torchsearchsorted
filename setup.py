17d5427619f48873cf6149c06ac6622777a25aa1
Antoine Liutkus <antoine.liutkus@inria.fr>
2019-04-23 13:00:04 +0200

    fixed a bug in setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,6 @@ import torch
 from setuptools import setup
 import torch.utils.cpp_extension as cpp
 
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 # In any case, include the CPU version
 modules = [cpp.CppExtension(
     'torchsearchsorted.cpu',
@@ -29,13 +19,11 @@ setup(
     name='torchsearchsorted',
     version='1.0',
     description='A searchsorted implementation for pytorch',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     keywords='searchsorted',
 
     author='Antoine Liutkus',
     author_email='antoine.liutkus@inria.fr',
-    packages=['torchsearchsorted torch'],
+    packages=['torchsearchsorted'],
     ext_modules=modules,
     cmdclass={
         'build_ext': cpp.BuildExtension
