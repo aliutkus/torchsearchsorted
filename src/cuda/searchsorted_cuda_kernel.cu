98916a7c890b7a4e53295aecdfc6ede659714231
Federico Baldassarre <baldassarre.fe@gmail.com>
2019-11-18 16:31:00 +0100

    CUDA version: correct impl of left/right and broadcasted tensors support

--- src/cuda/searchsorted_cuda_kernel.cu
+++ src/cuda/searchsorted_cuda_kernel.cu
@@ -1,142 +1,133 @@
+#include <ATen/cuda/detail/IndexUtils.cuh>
+#include <ATen/cuda/detail/TensorInfo.cuh>
+
 #include "searchsorted_cuda_kernel.h"
 
-template <typename scalar_t>
-__device__
-int eval(scalar_t val, scalar_t *a, int64_t row, int64_t col, int64_t ncol, bool side_left)
-{
-    /* Evaluates whether a[row,col] < val <= a[row, col+1]*/
-
-    if (col == ncol - 1)
-    {
-      // special case: we are on the right border
-      if (a[row * ncol + col] <= val){
-        return 1;}
-      else {
-        return -1;}
-    }
-    bool is_lower;
-    bool is_next_higher;
-
-    if (side_left) {
-      // a[row, col] < v <= a[row, col+1]
-      is_lower = (a[row * ncol + col] < val);
-      is_next_higher = (a[row*ncol + col + 1] >= val);
-    } else {
-      // a[row, col] <= v < a[row, col+1]
-      is_lower = (a[row * ncol + col] <= val);
-      is_next_higher = (a[row * ncol + col + 1] > val);
-    }
-    if (is_lower && is_next_higher) {
-        // we found the right spot
-        return 0;
-    } else if (is_lower) {
-    	// answer is on the right side
-        return 1;
-    } else {
-    	// answer is on the left side
-        return -1;
-    }
-}
 
 template <typename scalar_t>
 __device__
-int binary_search(scalar_t *a, int64_t row, scalar_t val, int64_t ncol, bool side_left)
-{
-  /* Look for the value `val` within row `row` of matrix `a`, which
-  has `ncol` columns.
-
-  the `a` matrix is assumed sorted in increasing order, row-wise
-
-  Returns 
-  * -1 if `val` is smaller than the smallest value found within that row of `a`
-  * `ncol` - 1 if `val` is larger than the largest element of that row of `a`
-  * Otherwise, return the column index `res` such that:
-    - a[row, col] < val <= a[row, col+1]. (if side_left), or 
-    - a[row, col] < val <= a[row, col+1] (if not side_left).
-   */
-
-  //start with left at 0 and right at number of columns of a
-  int64_t right = ncol;
-  int64_t left = 0;
-
-  while (right >= left) {
-      // take the midpoint of current left and right cursors
-      int64_t mid = left + (right-left)/2;
-
-      // check the relative position of val: are we good here ?
-      int rel_pos = eval(val, a, row, mid, ncol, side_left);
-      // we found the point
-      if(rel_pos == 0) {
-          return mid;
-      } else if (rel_pos > 0) {
-        if (mid==ncol-1){return ncol-1;}
-        // the answer is on the right side
-        left = mid;
-      } else {
-        if (mid==0){return -1;}
-        right = mid;
-      }
+int64_t bisect_left(scalar_t *array, scalar_t value, int64_t left, int64_t right) {
+/**
+ * Locate the insertion point of a value in a sorted array that would
+ * maintain the array sorted, i.e. the index i such that:
+ * array[i] <= value < array[i + 1]
+ * Only the index range [right, left) is considered.
+ *
+ * If the value is already present in the array, the returned index would
+ * insert the value to the left of any existing entry.
+ * If value is < than every element, the returned index is equal to left.
+ * If value is >=  than every element, the returned index is equal to right.
+ */
+  while (left < right) {
+    int64_t mid = (left + right) / 2;
+    if (value > array[mid]) {
+      left = ++mid;
+    } else {
+      right = mid;
+    }
   }
-  return -1;
+  return left;
 }
+
+
+template <typename scalar_t>
+__device__
+int64_t bisect_right(scalar_t *array, scalar_t value, int64_t left, int64_t right) {
+/**
+ * Locate the insertion point of a value in a sorted array that would
+ * maintain the array sorted, i.e. the index i such that:
+ * array[i] < value <= array[i + 1]
+ * Only the index range [right, left) is considered.
+ *
+ * If the value is already present in the array, the returned index would
+ * insert the value to the right of any existing entry.
+ * If value is <= than every element, the returned index is equal to left.
+ * If value is >  than every element, the returned index is equal to right.
+ */
+  while (left < right) {
+    int64_t mid = (left + right) / 2;
+    if (value >= array[mid]) {
+      left = ++mid;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+}
+
 
 template <typename scalar_t>
 __global__
 void searchsorted_kernel(
-  int64_t *res,
-  scalar_t *a,
-  scalar_t *v,
-  int64_t nrow_res, int64_t nrow_a, int64_t nrow_v, int64_t ncol_a, int64_t ncol_v, bool side_left)
-{
-    // get current row and column
-    int64_t row = blockIdx.y*blockDim.y+threadIdx.y;
-    int64_t col = blockIdx.x*blockDim.x+threadIdx.x;
+          at::cuda::detail::TensorInfo<scalar_t, int64_t> a,
+          at::cuda::detail::TensorInfo<scalar_t, int64_t> v,
+          at::cuda::detail::TensorInfo<int64_t, int64_t> res,
+          bool side_left) {
+  int64_t i = blockIdx.x * blockDim.x + threadIdx.x;
+  int64_t j = blockIdx.y * blockDim.y + threadIdx.y;
 
-    // check whether we are outside the bounds of what needs be computed.
-    if ((row >= nrow_res) || (col >= ncol_v)) {
-      return;}
+  if ((i >= res.sizes[0]) || (j >= res.sizes[1])) {
+    return;
+  }
 
-    // get the value to look for
-    int64_t row_in_v = (nrow_v==1) ? 0: row;
-    int64_t row_in_a = (nrow_a==1) ? 0: row;
-    int64_t idx_in_v = row_in_v*ncol_v+col;
-    int64_t idx_in_res = row*ncol_v+col;
+  // Search values in the range [left, right), i.e. an entire row of a
+  int64_t left = i * a.strides[0];
+  int64_t right = i * a.strides[0] + a.sizes[1];
 
-    // apply binary search
-    res[idx_in_res] = binary_search(a, row_in_a, v[idx_in_v], ncol_a, side_left)+1;
+  // idx_v is the location of the value in the flattened tensor v
+  // idx_res is the where the result will go in the flattened tensor res
+  int64_t idx_v = i * v.strides[0] + j * v.strides[1];
+  int64_t idx_res = i * res.strides[0] + j * res.strides[1];
+
+  // idx is the insertion index in the flattened tensor a
+  int64_t idx;
+  /* TODO this "if" works, but would be nicer to use function pointers:
+   * check side_left in searchsorted_cuda (on CPU) and pass the right pointer
+   * to the kernels (on GPU), but the fact that the bisect functions are
+   * templated and are defined with __device__ makes it hard to get the pointers
+   * right (the address on the CPU and on the GPU are different), see
+   * https://stackoverflow.com/questions/15644261/cuda-function-pointers
+  */
+  if (side_left) {
+    idx = bisect_left(a.data, v.data[idx_v], left, right);
+  } else {
+    idx = bisect_right(a.data, v.data[idx_v], left, right);
+  }
+  res.data[idx_res] = idx - i * a.strides[0];
 }
 
-
+__host__
 void searchsorted_cuda(
-  at::Tensor a,
-  at::Tensor v,
-  at::Tensor res,
-  bool side_left){
+        at::Tensor a,
+        at::Tensor v,
+        at::Tensor res,
+        bool side_left) {
+  // Kernel configuration:
+  // - 2D grid of size v.size(0) x v.size(1)
+  // - The grid is partitioned in blocks of 256 x 4
+  // - Each thread [i, j] will search for the value v[i, j] in the i-th row of a
+  dim3 threads(256, 4);
+  dim3 blocks(
+    (v.size(0) + threads.x - 1) / threads.x,
+    (v.size(1) + threads.y - 1) / threads.y
+  );
 
-      // Get the dimensions
-      auto nrow_a = a.size(/*dim=*/0);
-      auto nrow_v = v.size(/*dim=*/0);
-      auto ncol_a = a.size(/*dim=*/1);
-      auto ncol_v = v.size(/*dim=*/1);
+  AT_DISPATCH_ALL_TYPES(a.type(), "searchsorted cuda", ([&] {
+    /* Related to the comment in searchsorted_kernel, getting the address of a
+     * __device__ function from a __host__ function isn't straightforward,
+     * but here's a start
+    */
+    // int64_t (*bisect)(scalar_t*, scalar_t, int64_t, int64_t);
+    // if (side_left) {
+    //   bisect = &bisect_left<scalar_t>;
+    // } else {
+    //   bisect = &bisect_right<scalar_t>;
+    // }
 
-      auto nrow_res = fmax(double(nrow_a), double(nrow_v));
-
-      // prepare the kernel configuration
-      dim3 threads(ncol_v, nrow_res);
-      dim3 blocks(1, 1);
-      if (nrow_res*ncol_v > 1024){
-         threads.x = int(fmin(double(1024), double(ncol_v)));
-         threads.y = floor(1024/threads.x);
-         blocks.x = ceil(double(ncol_v)/double(threads.x));
-         blocks.y = ceil(double(nrow_res)/double(threads.y));
-      }
-
-      AT_DISPATCH_ALL_TYPES(a.type(), "searchsorted cuda", ([&] {
-        searchsorted_kernel<scalar_t><<<blocks, threads>>>(
-          res.data<int64_t>(),
-          a.data<scalar_t>(),
-          v.data<scalar_t>(),
-          nrow_res, nrow_a, nrow_v, ncol_a, ncol_v, side_left);
-      }));
-
+    searchsorted_kernel<scalar_t><<<blocks, threads>>>(
+      at::cuda::detail::getTensorInfo<scalar_t, int64_t>(a),
+      at::cuda::detail::getTensorInfo<scalar_t, int64_t>(v),
+      at::cuda::detail::getTensorInfo<int64_t, int64_t>(res),
+      side_left);
+  }));
   }
